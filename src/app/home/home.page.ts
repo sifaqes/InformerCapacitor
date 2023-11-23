3827ce6c9292b44f4a4a05778da31675e99d4239
SIFAQES <siphaxzer@gmail.com>
2023-11-23 20:15:13 +0100

    set template

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
 
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      App.exitApp();
+      }); 
+  }
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
